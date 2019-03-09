8f0530517b699335b27915d3cc05629c20fc1da7
yuzi <hayuzi@163.com>
2019-03-09 17:18:29 +0800

    init

---
+++ src/pages/admin/dashboard/Dashboard.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Row, Col, Breadcrumb} from 'antd';
+// import {NavLink} from 'react-router-dom';
+import styles from '@/pages/admin/dashboard/Dashboard.module.less';
+
+class Dashboard extends Component {
+
+  render() {
+    return (
+      <Row>
+        <Col span={24} className={styles.breadCrumb}>
+          <Breadcrumb>
+            <Breadcrumb.Item>首页</Breadcrumb.Item>
+            <Breadcrumb.Item>dashboard</Breadcrumb.Item>
+          </Breadcrumb>
+        </Col>
+        <Col span={24}>
+
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default Dashboard;
--- src/router/Router.js
+++ src/router/Router.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import NotFound from '@/pages/NotFound';
 import Login from '../pages/User/Login';
 import BasicLayout from "@/components/layout/BasicLayout";
+import AdminLayout from "@/components/layout/AdminLayout";
 import IndexPage from "@/pages/IndexPage";
 import BlankPage from "@/pages/frontend/article/BlankPage";
 import ArticleDetail from "@/pages/frontend/article/ArticleDetail";
+import DashBoard from "@/pages/admin/dashboard/Dashboard"
 // import asyncComponent from "@/router/asyncComponent";
 // // react 按照路由异步加载组件
 // const IndexPage=asyncComponent(()=>import(/* webpackChunkName: "index-page" */"@/pages/IndexPage"));
@@ -15,21 +17,36 @@ import ArticleDetail from "@/pages/frontend/article/ArticleDetail";
 // const Login=asyncComponent(()=>import(/* webpackChunkName: "user-login" */"@/pages/User/Login"));
 // const BasicLayout=asyncComponent(()=>import(/* webpackChunkName: "basic-layout" */"@/components/layout/BasicLayout"));
 
-const LayoutBox = ({ match }) => {
+const LayoutBox = ({match}) => {
   if (match.url === '/') {
     match.url = '';
   }
   return (
     <BasicLayout>
       <Switch>
-        <Route exact path={`${match.url}/`} component={ IndexPage }/>
-        <Route exact path={`${match.url}/article`} component={ ArticleDetail }/>
-        <Route exact path={`${match.url}/blank`} component={ BlankPage }/>
-        <Route exact path={`${match.url}/sample`} component={ BlankPage }/>
-        <Route exact path="/404" component={ NotFound } />
-        <Route exact component={ NotFound } />
+        <Route exact path={`${match.url}/`} component={IndexPage}/>
+        <Route exact path={`${match.url}/article`} component={ArticleDetail}/>
+        <Route exact path={`${match.url}/blank`} component={BlankPage}/>
+        <Route exact path="/404" component={NotFound}/>
+        <Route exact component={NotFound}/>
       </Switch>
     </BasicLayout>
+  );
+};
+
+
+const AdminLayoutBox = ({match}) => {
+  return (
+    <AdminLayout>
+      <Switch>
+        <Route exact path={`${match.url}/dashboard`} component={DashBoard}/>
+        <Route exact path={`${match.url}/article/list`} component={IndexPage}/>
+        <Route exact path={`${match.url}/article/tags`} component={IndexPage}/>
+        <Route exact path={`${match.url}/user/list`} component={BlankPage}/>
+        <Route exact path="/404" component={NotFound}/>
+        <Route exact component={NotFound}/>
+      </Switch>
+    </AdminLayout>
   );
 };
 
@@ -39,19 +56,9 @@ class RouterConfig extends Component {
     return (
       <Router>
         <Switch>
-          <Route path="/auth">
-            <Switch>
-              <Route exact path="/auth/login" component={ Login }/>
-              <Route exact component={ NotFound }/>
-            </Switch>
-          </Route>
-          <Route path="/admin">
-            <Switch>
-              <Route exact path="/admin/login" component={ Login }/>
-              <Route exact component={ NotFound }/>
-            </Switch>
-          </Route>
-          <Route path="/" component={ LayoutBox } />
+          <Route path="/login" component={Login}/>
+          <Route path="/admin" component={AdminLayoutBox}/>
+          <Route path="/" component={LayoutBox}/>
         </Switch>
       </Router>
     );
