d5fe4433562f6dc71dfd63a48bd8bc1cd52b11f5
yuzi <hayuzi@163.com>
2019-03-09 21:14:51 +0800

    init

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,8 @@ import user from '@/models/frontened/user';
 import tag from '@/models/frontened/tag';
 import article from '@/models/frontened/article';
 import comment from '@/models/frontened/comment';
-import adminUser from '@/models/admin/adminUser';
 
 const models = {
-  adminUser,
   user,
   tag,
   article,
