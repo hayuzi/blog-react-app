beee8ec1a2a5162b331e104e2a08757083d88590
yuzi <hayuzi@163.com>
2019-12-14 10:22:10 +0800

    修改备案号

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,7 +7,9 @@ class Footer extends Component {
       <div className={ styles.container }>
         <div className= { styles.content }>
           <div className={ styles.icp }>
-            苏ICP备17049372号
+            <a href="http://beian.miit.gov.cn" target="_blank">
+              苏ICP备17049372号
+            </a>
           </div>
         </div>
       </div>
